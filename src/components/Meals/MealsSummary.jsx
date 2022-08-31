28f581638b739f867d8260c7df1a19cc0f4eb19f
Stav <stavush93@gmail.com>
2022-08-31 22:36:34 +0300

    adding meals components

---
+++ src/components/Meals/MealsSummary.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./MealsSummary.module.css"
+
+function MealsSummary() {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea magnam
+        delectus, voluptatum magni maiores esse?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea magnam
+        delectus, voluptatum magni maiores esse?
+      </p>
+    </section>
+  );
+}
+
+export default MealsSummary;

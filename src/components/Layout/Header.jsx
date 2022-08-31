38de05e91537ae559428612c4c6e5aaa294dc449
Stav <stavush93@gmail.com>
2022-08-31 21:37:50 +0300

    initial commit

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import mealsImg from "../../assets/meals.jpg"
+import classes from "./Header.module.css"
+
+function Header() {
+
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="" />
+      </div>
+    </>
+  )
+}
+
+export default Header

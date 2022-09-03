5a540f691805b8a3a28ef8e024fc37c36c7d10c0
Stav <stavush93@gmail.com>
2022-09-03 16:53:20 +0300

    starting the cartReducer - handeling cart state

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import classes from "./HeaderCartButton.module.css"
 import CartIcon from "../Cart/CartIcon"
+import CartContext from "../../context/cart-context"
 
-function HeaderCartButton({onClick}) {
+function HeaderCartButton({ onClick }) {
+  const cartCtx = useContext(CartContext);
+  const cartItemsNum = cartCtx.items.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
@@ -10,7 +16,7 @@ function HeaderCartButton({onClick}) {
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>
-        [cart items]
+        {cartItemsNum}
       </span>
     </button>
   )
--- src/context/cart-provider.js
+++ src/context/cart-provider.js
@@ -1,18 +1,43 @@
+import { useReducer } from 'react'
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+}
+
+const cartReducer = (state, action) => {
+  switch(action.type) {
+    case "ADD_ITEM": 
+      // we use concat insted of push to get a new array
+      const updatedItems = state.items.concat(action.item)
+      const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount
+      }
+    case "REMOVE_ITEM": 
+      //
+      break;
+    default:
+      return defaultCartState
+  }
+}
+
 const CartProvider = ({ children }) => {
+  const [cartState, dispatch] = useReducer(cartReducer ,defaultCartState)
 
   const addItemToCartHandler = (item) => {
-
+    dispatch({type: "ADD_ITEM", item})
   }
 
   const removeItemFromCartHandler = (id) => {
-
+    dispatch({type: "REMOVE_ITEM", id})
   }
 
   const cartContext = { 
-    items: [],
-    totalItems: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   }
